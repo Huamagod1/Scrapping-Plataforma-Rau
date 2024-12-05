57605e12ae0ba9ff3cc6a03ecf7914716c3b5524
Huamagod1 <f.andres.h.zuniga@gmail.com>
2024-12-05 18:00:15 -0300

    Recopilacion de datos se logra, falta pulir la navegacion entre los filtros, ya que al usar filtro por estado de formacion y tipo de formacion da error

--- utils/scrape.py
+++ utils/scrape.py
@@ -3,83 +3,129 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 
+
+
+
 def scrape(driver, config):
     """
     Funcion para navegar y extraer datos de la pagina despues del inicio de sesion.
 
     """
     try:
-        menu_button = WebDriverWait(driver, 5).until(
+        menu_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.ID, "ctl00_header_headerResponsive_lnkNavBar"))
         )
         menu_button.click()
 
-        opcion_learning = WebDriverWait(driver, 5).until(
+        opcion_learning = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.ID, "ctl00_header_headerResponsive_responsiveNav_rptMenu_ctl02_lnkMenu"))
         )
         opcion_learning.click()
 
-        learning = WebDriverWait(driver, 5).until(
+        learning = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.ID, "ctl00_header_headerResponsive_responsiveNav_rptMenu_ctl02_rptSubMenu_ctl02_lnkSubMenu"))
         )
         learning.click()
         
-        
-        filtro_activo = WebDriverWait(driver, 5).until(
+        #Colocar filtro por estado de formacion
+
+        filtro_estado_formacion = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.ID, "_bj_j"))
         )
-        filtro_activo.click()
-        print("Se presiona en el filtro y muestra las opciones")
-        #Selecciona el filtro todos, estado de formacion
-        filtro_todos = WebDriverWait(driver, 5).until(
+        filtro_estado_formacion.click()
+        print("Se presiona en el filtro por estado de formacion, muestra las opciones")
+
+        filtro_estado_formacion_todos = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.ID, "_bj_j_dd_item_1"))
         )
-        filtro_todos.click()
-        print("Se selecciona la opcion todos")
-        
+        filtro_estado_formacion_todos.click()
+        print("Se presiona en el filtro por estado de formacion, muestra las opciones")
+
+
+        WebDriverWait(driver, 20).until(
+            EC.invisibility_of_element_located((By.CLASS_NAME, "modal_wait"))
+        )
+        print("El modal de carga ha desaparecido")
 
 
 
-        #Filtra por tipo de formacion
-        filtro_formacion = WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-tag='dropdownlist-tr-SelectedLoType']"))
+
+
+
+
+
+        #Filtro tipo de formacion
+        filtro_formacion = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "_bj_p"))
         )
         filtro_formacion.click()
-
-        itinerario_formativo = WebDriverWait(driver,5).until(
-            EC.element_to_be_clickable((By.ID,"_bj_p_dd_item_3"))
+        print("Se presiona en el filtro y muestra las opciones")
+        
+        #Selecciona el filtro todos, estado de formacion y selecciona itinerario formativo
+        filtro_itinerario = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "_bj_p_dd_item_3"))
         )
-        itinerario_formativo.click()
+        filtro_itinerario.click()
+        print("Se seleccionan los itinerarios formativos")
+            
+        
+        WebDriverWait(driver, 20).until(
+            EC.invisibility_of_element_located((By.CLASS_NAME, "modal_wait"))
+        )
+        print("El modal de carga ha desaparecido")
+        
+        
+        WebDriverWait(driver,20).until(
+            EC.presence_of_all_elements_located((By.ID,"__u"))
+        )
+
+        print("La pagina ha terminado de cargar")
+
 
 
 
         
         #Extraer itinerraios formativos y sus estados
         itinerarios = []
-        elementos = driver.find_elements(By.CSS_SELECTOR, "a[id$='_k_f']")
-        print("Se encontraron los siguientes itinerarios: " + elementos)
+        contenedores = driver.find_elements(By.CSS_SELECTOR, "div[id$='k_e']")
+        if not contenedores:
+            raise Exception("No se encontraron los contenendores de itinerarios.")
+
+        print(f"Se encontraron {len(contenedores)} contenedores de itinerarios.")
+        #´Patro rial itinerarios BLOQUES : _bjbi0k_e, _bjbi11k_e, _bjbi12k_e
+        #Curso online: _bjbi1b_e, _bjbi14b_e,
+
         #patron para los nombres de los itinerarios: _bjbi0k_k_f, _bjbi1k_k_f, _bjbi2k_k_f
+        #Patron nombres Curso online: _bjbi1b_k_f, _bjbi2b_k_f, 
         #Patron para los estados de los itinerarios: _bjbi0k_kbb, _bjbi1k_kbb, _bjbi2k_kbb
 
-        for elemento in elementos:
+        for contenedor in contenedores:
             try:
                 
-                # Extraer el nombre del curso
-                nombre_curso = elemento.text.strip()
-                print("Nombre Curso" + nombre_curso)
-                # Construir el ID del estado a partir del ID del itinerario
-                id_estado = elemento.get_attribute("id").replace("_k_f", "_k_kbb")
+                contenedor_id = contenedor.get_attribute("id")
+                print("ID contenedor encontrado: "+  contenedor_id)
 
-                # Buscar el elemento del estado usando el ID generado
+                # Se construyen los IDS A BUSCAR
+                id_nombre = contenedor_id.replace("k_e", "k_k_f")
+                id_estado = contenedor_id.replace("k_e", "k_kbb")
+
+                #Se busca el nombre del itinerario
+                nombre_elemento = driver.find_element(By.ID, id_nombre)
+                nombre_curso = nombre_elemento.text.strip()
+                print("Curso Encontrado: " + nombre_curso)
+                #Busca el estado actual del itinerario
+
                 estado_elemento = driver.find_element(By.ID, id_estado)
                 estado_curso = estado_elemento.text.strip()
+                print("Estado del Curso: "+ estado_curso)
+
 
                 # Guardar los datos en la lista
                 itinerarios.append({"Nombre del curso": nombre_curso, "Estado del curso": estado_curso})
 
             except Exception as e:
                 # Manejar casos en los que no se pueda encontrar el estado
-                print(f"No se pudo extraer el estado para el itinerario con ID {elemento.get_attribute('id')}: {e}")
+                print(f"No se pudo extraer el estado para el itinerario con ID {contenedor_id}: {e}")
 
 
 
@@ -106,15 +152,6 @@ def scrape(driver, config):
  
 
       
-
-
-
-
-
-      
-
-
-#def filtro_itinerarios_activos(driver,config):
 
         
     
